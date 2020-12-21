2dc00484f83501b800ec20ea8f1592c8c1710958
pipkamv <kanapmbm@gmail.com>
2020-12-21 19:09:17 +0600

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from django.contrib import admin
 
 urlpatterns = [
-    path('', views.homePageView, name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
 ]
+
 
 
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-    return HttpResponse('Hello World!')
 
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
