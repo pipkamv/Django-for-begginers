b46fb2b15e4a1fefad82103e10c46ede7f17d652
pipkamv <kanapmbm@gmail.com>
2020-12-22 10:03:40 +0600

    New updates for Heroku deployment

--- test_project/settings.py
+++ test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'no03_l*3fnezsy-59==did6_xam%z@(h9wfjpt_&_!2cb*za*#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
